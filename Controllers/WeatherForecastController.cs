9843c08bbeadc3ceba0c4af68a07e57b755a87c0
vrushalhw <vrushal.darkunde@thehelixware.com>
2026-08-22 18:01:33 +0530

    Change weather forecast count from 5 to 3
    Updated the number of weather forecasts returned from 5 to 3.

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -14,7 +14,7 @@ namespace WEB_APIS_10.Controllers
         [HttpGet(Name = "GetWeatherForecast")]
         public IEnumerable<WeatherForecast> Get()
         {
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            return Enumerable.Range(1, 3).Select(index => new WeatherForecast
             {
                 Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
                 TemperatureC = Random.Shared.Next(-20, 55),
